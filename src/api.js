028b6495fb23c34bbcd01d8816cf8a91db5674e7
Saroj Rana <rana.saroj95@gmail.com>
2022-03-14 15:27:11 +0545

    Fix server endpoint and response.

--- src/api.js
+++ src/api.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
-const IPO_CHECK_API = 'https://iporesult.cdsc.com.np/result/result/check';
-const COMPANY_LIST_API = 'https://iporesult.cdsc.com.np/result/companyShares/fileUploaded';
+const BASE_URL = 'https://ipo-check-server.herokuapp.com';
+const IPO_CHECK_API = 'https://ipo-check-server.herokuapp.com/check-ipo';
+const COMPANY_LIST_API = 'https://ipo-check-server.herokuapp.com/company-list';
 
 export const getCompanyList = async () => {
   try {
     const response = await axios.get(COMPANY_LIST_API);
 
-    return response.data.body;
+    return response.data;
   } catch (err) {
     console.log(err);
   }
